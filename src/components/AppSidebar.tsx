b7c034c00cdd7540d5271ce062c04bb795f36db9
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-11-14 11:23:13 -0300

    feat: add sidebar component with responsive design and styling updates- Introduces a new sidebar component with context for managing its state.- Implements sidebar menu with navigation items.- Adds styles for sidebar and related components in `globals.css`.- Updates package dependencies in `package.json` for new components

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,70 @@
+import { ChartNoAxesColumn, Map, PencilLine, ScrollText } from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarSeparator,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+    {
+        title: "Início",
+        url: "dashboard",
+        icon: ChartNoAxesColumn,
+    },
+    {
+        title: "Gestão de Territórios",
+        url: "gestao",
+        icon: Map,
+    },
+    {
+        title: "Cadastros",
+        url: "#",
+        icon: PencilLine,
+    },
+    {
+        title: "Relatórios",
+        url: "#",
+        icon: ScrollText,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" className="mt-8">
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <>
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <a href={item.url} className="flex gap-3 my-1">
+                                                <item.icon
+                                                    style={{
+                                                        width: "1.5rem",
+                                                        height: "1.5rem"
+                                                    }}
+                                                    className="text-primary" />
+                                                <span className="text-lg">{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                            
+                                </>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
