5df55f746a78c27fd348726f7b71f7fc2ad1d351
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-11-14 20:57:15 -0300

    feat: add RoundMenu component and dialog integration for new round initiation functionality

---
+++ src/components/RoundMenu.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
+export function RoundMenu() {
+  const [name, setName] = useState("")
+  const [roundType, setRoundType] = useState<'padrao' | 'cartas' | 'campanha'>("padrao")
+
+  const colorTheme = {
+    'padrao': "#7AAD58",
+    'cartas': "#E29D4F",
+    'campanha': '#5B98AB'
+  }
+
+  return (
+    <>
+      <h2 className="text-2xl font-medium text-center">Iniciar nova rodada</h2>
+
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="name">Nome da rodada</Label>
+          <Input
+            id="name"
+            placeholder="Digite o nome da rodada"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="game-type">Tipo de rodada</Label>
+          <Select value={roundType} onValueChange={(value: string) => setRoundType(value as 'padrao' | 'cartas' | 'campanha')}>
+            <SelectTrigger id="game-type">
+              <SelectValue placeholder="Selecione o tipo da rodada" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="padrao">Padrão</SelectItem>
+              <SelectItem value="cartas">Cartas</SelectItem>
+              <SelectItem value="campanha">Campanha</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
+      <Button
+        className="w-full mt-4 text-white"
+        variant="secondary"
+        style={
+          {
+            backgroundColor: colorTheme[roundType] || "#000"
+          }
+        }
+        disabled={!name || !roundType}
+      >
+        Iniciar rodada
+      </Button>
+    </>
+
+  )
+}
--- src/components/SearchInterface.tsx
+++ src/components/SearchInterface.tsx
@@ -4,37 +4,47 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Search } from 'lucide-react'
 
-import { ChangeEventHandler, MouseEventHandler } from 'react';
+import { ChangeEventHandler } from 'react';
+import { RoundMenu } from "./RoundMenu";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
-export function SearchInterface({ 
+export function SearchInterface({
   onSearchChange, // Evento para mudança no input
-  onButtonClick,  // Evento para clique do botão
   searchValue,    // Valor do input
   buttonLabel = "Criar Novo" // Label padrão do botão
 }: {
   onSearchChange: ChangeEventHandler<HTMLInputElement>,
-  onButtonClick: MouseEventHandler<HTMLButtonElement>,
   searchValue: string,
   buttonLabel?: string
 }) {
   return (
-    <div className="w-full flex items-center justify-between gap-4 pt-6 ">
-      <div className="relative">
-        <Input
-          type="search"
-          placeholder="Pesquise"
-          value={searchValue} // Passando valor do input
-          onChange={onSearchChange} // Chamando evento no pai
-          className="pl-10 bg-background border rounded-md"
-        />
-        <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+      <div className="w-full flex items-center justify-between gap-4 pt-6 ">
+        <div className="relative">
+          <Input
+            type="search"
+            placeholder="Pesquise"
+            value={searchValue} // Passando valor do input
+            onChange={onSearchChange} // Chamando evento no pai
+            className="pl-10 bg-background border rounded-md"
+          />
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              className="bg-[#6BA84F] hover:bg-[#6BA84F]/90 text-white"
+            >
+              {buttonLabel}
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <RoundMenu></RoundMenu>
+          </DialogContent>
+        </Dialog>
       </div>
-      <Button 
-        className="bg-[#6BA84F] hover:bg-[#6BA84F]/90 text-white"
-        onClick={onButtonClick} // Chamando evento de clique no pai
-      >
-        {buttonLabel}
-      </Button>
-    </div>
   )
 }
