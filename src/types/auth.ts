cbe1080839bdaa92160e3f377b79258cc79debf9
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-02-28 15:37:30 -0300

    feat(types): add DecodedToken interface and restructure AuthContextType

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,12 +1,18 @@
 // types/auth.ts
 export interface User {
-    token: string;
-  }
-  
-  export interface AuthContextType {
-    user: User | null;
-    loading: boolean;
-    login: (email: string, password: string) => Promise<void>;
-    logout: () => void;
-  }
-  
+  token: string;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+export interface DecodedToken {
+  email: string;
+  userName: string;
+  roles: string[];
+  tenantId: number;
+}
